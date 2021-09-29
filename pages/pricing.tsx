34b1ff6e979b7f84f50fb49b432a3900b09372ed
Jorge Acero <julianacero98@gmail.com>
2021-09-29 18:14:21 -0500

    ComponentShield implemented

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,9 +1,25 @@
+import { ComponentShield } from 'next-shield'
 import { Layout } from '@/components/routes/Layout'
+import { useAuth } from '@/hooks/auth'
+import { Metadata } from '@/types/User'
 
 export default function Pricing() {
+  const { user, isAuth, isLoading } = useAuth()
+  const userMetadata = user?.user_metadata as Metadata
+
   return (
     <Layout title="Pricing">
       <h1>Pricing</h1>
+
+      <ComponentShield showIf={isAuth && !isLoading}>
+        <p>You are authenticated</p>
+      </ComponentShield>
+      <ComponentShield RBAC showForRole="ADMIN" userRole={userMetadata?.role}>
+        <p>You are an ADMIN</p>
+      </ComponentShield>
+      <ComponentShield RBAC showForRole="EMPLOYEE" userRole={userMetadata?.role}>
+        <p>You are an EMPLOYEE</p>
+      </ComponentShield>
     </Layout>
   )
 }
