5b84e5798440db19553669871f94cb9365f7a548
Jorge Acero <julianacero98@gmail.com>
2021-10-20 21:27:29 -0500

    removed unused Clerk components and

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,12 @@
 import type { AppProps } from 'next/app'
 
-import { useRouter } from 'next/router'
-import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn } from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs'
 
 import { Shield } from '@/components/routes/Shield'
 
 import '@/styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const { pathname } = useRouter()
-  const publicPages = ['/']
-  const isPublicPage = publicPages.includes(pathname)
-
   return (
     <ClerkProvider>
       <Shield>
