c93ecb74d1f51dc4bc21b1c26efe160d2d7d1c01
Jorge Acero <julianacero98@gmail.com>
2021-10-21 10:59:44 -0500

    ComponentShield added

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,9 +1,27 @@
+import { useUser } from '@clerk/clerk-react'
+import { ComponentShield } from 'next-shield'
+
 import { Layout } from '@/components/routes/Layout'
+import { Metadata } from '@/types/User'
 
 export default function Pricing() {
+  const { user, isLoading, isSignedIn } = useUser({ withAssertions: true })
+  const userMetadata: Metadata = user?.publicMetadata
+
   return (
     <Layout title="Pricing">
       <h1>Pricing</h1>
+
+      <ComponentShield
+        showIf={isSignedIn(user) && !isLoading(user)}
+        fallback={<p>You are unauthorized</p>}
+      >
+        <p>You are authorized</p>
+      </ComponentShield>
+
+      <ComponentShield RBAC showForRole="ADMIN" userRole={userMetadata?.role}>
+        <p>You are an admin</p>
+      </ComponentShield>
     </Layout>
   )
 }
